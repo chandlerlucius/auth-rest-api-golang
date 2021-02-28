30872f39fb52a0b50f07999b22fb65afda493e94
Chandler Lucius <clucius@chandlerlucius.com>
2021-02-28 16:24:06 -0600

    Added login handler Extrapolated input checking methods Added/Updated bcrypt password hashing methods

--- server/auth.go
+++ server/auth.go
@@ -26,36 +26,17 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	username := auth.Username
-	password := auth.Password
-	confirmPassword := auth.ConfirmPassword
-
-	if len(username) < 3 || len(password) < 3 {
-		message := Message{http.StatusBadRequest, "Failure", "Username and Password must longer than 2 characters.", "", 0}
-		sendMessageAndLogResult(w, message, nil)
+	validInputs := checkInputs(w, auth)
+	if !validInputs {
 		return
 	}
 
-	if password != confirmPassword {
-		message := Message{http.StatusBadRequest, "Failure", "Passwords must be the same.", "", 0}
-		sendMessageAndLogResult(w, message, nil)
+	hashedPassword := hashPassword(w, auth)
+	if hashedPassword == "" {
 		return
 	}
 
-	user, err := findUserInDb(username)
-	if err != nil && err != mongo.ErrNoDocuments {
-		sendInternalError(w, err)
-		return
-	}
-
-	if (User{}) != user {
-		message := Message{http.StatusBadRequest, "Failure", "Username already exists! Try a different username please.", "", 0}
-		sendMessageAndLogResult(w, message, nil)
-		return
-	}
-
-	hashedPassword := hashPassword(password)
-	res, err := addUserToDb(username, hashedPassword)
+	res, err := addUserToDb(auth.Username, hashedPassword)
 	if err != nil || res == nil {
 		sendInternalError(w, err)
 		return
@@ -65,12 +46,85 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 	sendMessageAndLogResult(w, message, nil)
 }
 
-func hashPassword(password string) string {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	decoder := json.NewDecoder(r.Body)
+	var auth Auth
+	err := decoder.Decode(&auth)
 	if err != nil {
-		log.Print(err)
+		sendInternalError(w, err)
+		return
+	}
+
+	user, err := findUserInDb(auth.Username)
+	if err != nil && err != mongo.ErrNoDocuments {
+		sendInternalError(w, err)
+		return
+	}
+
+	if (User{}) == user {
+		message := Message{http.StatusBadRequest, "Failure", "Username " + auth.Username + " does not exist!", "", 0}
+		sendMessageAndLogResult(w, message, nil)
+		return
+	}
+
+	validPassword := checkPassword(w, auth.Password, user.Password)
+	if !validPassword {
+		return
+	}
+
+	message := Message{http.StatusOK, "Success", "User " + user.Username + " has logged in!", "", 0}
+	sendMessageAndLogResult(w, message, nil)
+}
+
+func checkInputs(w http.ResponseWriter, auth Auth) bool {
+	username := auth.Username
+	password := auth.Password
+	confirmPassword := auth.ConfirmPassword
+
+	if len(username) < 3 || len(password) < 3 {
+		message := Message{http.StatusBadRequest, "Failure", "Username and Password must longer than 2 characters.", "", 0}
+		sendMessageAndLogResult(w, message, nil)
+		return false
+	}
+
+	if password != confirmPassword {
+		message := Message{http.StatusBadRequest, "Failure", "Passwords must be the same.", "", 0}
+		sendMessageAndLogResult(w, message, nil)
+		return false
+	}
+
+	user, err := findUserInDb(username)
+	if err != nil && err != mongo.ErrNoDocuments {
+		sendInternalError(w, err)
+		return false
+	}
+
+	if (User{}) != user {
+		message := Message{http.StatusBadRequest, "Failure", "Username " + user.Username + " already exists! Try a different username please.", "", 0}
+		sendMessageAndLogResult(w, message, nil)
+		return false
+	}
+
+	return true
+}
+
+func hashPassword(w http.ResponseWriter, auth Auth) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(auth.Password), 14)
+	if err != nil {
+		sendInternalError(w, err)
+		return ""
 	}
 	return string(bytes)
+}
+
+func checkPassword(w http.ResponseWriter, attemptedPassword string, storedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(storedPassword), []byte(attemptedPassword))
+	if err != nil {
+		sendInternalError(w, err)
+		return false
+	}
+	return true
 }
 
 func sendInternalError(w http.ResponseWriter, err error) {
