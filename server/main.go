302a91a91953361a448b3c0037a4b37672f70387
Chandler Lucius <clucius@chandlerlucius.com>
2021-02-28 14:12:24 -0600

    Added basic http routing and response message struct

---
+++ server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", signupHandler).Methods("POST")
+
+	http.Handle("/", router)
+	http.ListenAndServe(":9000", router)
+}
+
+// Message is used to send a title and body as error message to client
+type Message struct {
+	Status  int    `json:"status"`
+	Title   string `json:"title"`
+	Body    string `json:"body"`
+	Token   string `json:"token"`
+	Timeout int64  `json:"timeout"`
+}
